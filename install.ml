94fe2e9ba4af4755e3023f4ff051484c01e1cf15
Jeremie Dimino <jdimino@janestreet.com>
2016-07-19 08:54:08 +0100

    114.02+05

--- install.ml
+++ install.ml
@@ -4,8 +4,7 @@
 open Oasis2opam_install;;
 
 generate ~package:"typerep_extended"
-  [ oasis_lib "json_typerep"
-  ; oasis_lib "typerep_bin_io"
+  [ oasis_lib "typerep_bin_io"
   ; oasis_lib "typerep_experimental"
   ; oasis_lib "typerep_extended"
   ; oasis_lib "typerep_sexp"
